2fa2c12d4f74bad4e762b8a08402ce93fe4de766
belobruna <bruna.belo@gmail.com>
2021-06-21 00:24:17 -0300

    index.js e cipher.js funcionando

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+function encode (e) {
+    e.preventDefault();
+
+    let string = document.getElementById("input-encrypt").value;
+    const offset = Number(document.getElementById("input-offset").value);
+    const encryptedMsg = cipher.encode(offset, string);
+    document.getElementById("input-decrypt").value = encryptedMsg;
+} 
+
+function decode(e) {
+    e.preventDefault();
+    
+     let string = document.getElementById("input-decrypt").value;
+     const offset = Number(document.getElementById("input-offset").value);
+     const decryptedMsg = cipher.decode(offset, string);
+     document.getElementById("input-encrypt").value = decryptedMsg;
+}
+
+
+let btEncrypt = document.getElementById("bt-encrypt");
+btEncrypt.addEventListener("click", encode);
+
+let btDecrypt = document.getElementById("bt-decrypt");
+btDecrypt.addEventListener("click", decode);
